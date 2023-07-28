3515b239d7ce1583fd85a7d1fe2a1222341b440d
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-07-28 16:34:25 +0200

    converted btnClick from normal fn to arrow fn

--- js/main.js
+++ js/main.js
@@ -13,12 +13,13 @@ const app = createApp({
   methods: {
     btnClick(){
       for(let i = 0; i < 10; i++){
-      axios.get(url).then(function (axiosResponse){
-        let randomEmails = [];
-        let generatedEmail = axiosResponse.data.response;
-        randomEmails.push(generatedEmail);
-        console.log(randomEmails);
-        });
+      axios
+        .get(url)
+        .then((axiosResponse) => {
+          let generatedEmail = axiosResponse.data.response;
+          console.log(generatedEmail)
+          this.randomEmails.push(generatedEmail);
+          });
       }
     }
   },
